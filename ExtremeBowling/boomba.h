c530d9c35a4c6d4805e38394978a8155368cd97b
Joseph Lee <leej240@mcmaster.ca>
2021-11-07 11:22:39 -0500

    Update boomba, pin, power up, and sweeper header files

--- ExtremeBowling/boomba.h
+++ ExtremeBowling/boomba.h
@@ -3,9 +3,26 @@
 #ifndef BOOMBA_H
 #define BOOMBA_H
 
+/* 
+The Boomba class will be similar to the Goomba from Mario. It will be a character
+that moves around randomly at a slow speed and will negatively affect the player 
+if the player hits it. Ex. we could have the player be stunned and not be able to 
+move for a certain amount of time after colliding with a Boomba, or we could have
+the player bounce off of the Boomba and lose pins, or a life.
+*/
+
 class Boomba {
     private:
+        PhysicsObject3D boombaPhysics;
+        // Mesh *boombaMesh;
+        // Texture boombaTexture;
     public:
+        float getX();
+        float getY();
+        float getZ();
+
+        void defaultMovement(); // random, slow movement, should not move off map by itself, avoid powerups?
+        void collisionAnimation(); // will depend on if player has invisible powerup or not
 };
 
 #endif
--- ExtremeBowling/sweeper.h
+++ ExtremeBowling/sweeper.h
@@ -3,9 +3,23 @@
 #ifndef SWEEPER_H
 #define SWEEPER_H
 
+/* 
+The sweeper class will stay in one place, and will have a continuous movement that 
+will move the player off the map if the player collides with it.
+*/
+
 class Sweeper {
     private:
+        PhysicsObject3D sweeperPhysics;
+        // Mesh *sweeperMesh;
+        // Texture sweeperTexture;
     public:
+        float getX();
+        float getY();
+        float getZ();
+
+        void defaultMovement(); // will need to loop some movement
+        void collisionAnimation(); // don't necessarily need this
 };
 
 #endif
