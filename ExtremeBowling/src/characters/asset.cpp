8ce840ce399b6dd04151253b0dea31692233fab8
azadis2 <azadis2@mcmaster.ca>
2021-11-12 14:29:28 -0500

    added objLoader to asset

--- ExtremeBowling/src/characters/asset.cpp
+++ ExtremeBowling/src/characters/asset.cpp
@@ -1,4 +1,16 @@
 #include "asset.h"
+#include "../misc/mathlib_3D.h"
+#include "../misc/physics_3D.h"
+#include <fstream>
+#include <sstream>
+#include <stdio.h> 
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+#include <iostream>
+#include <cstring>
+#include <string>
+
 
 
 
@@ -22,4 +34,88 @@ float Asset::getZ(){
     this->physics.getPos().z;
 }
 
+// Load the obj file
+bool Asset::loadObj(const char* filename)
+{
+    // counters
+    int pos = 0;
+    int tex = 0;
+    int norm = 0;
+    int face = 0;
 
+    // open the file
+    std::ifstream openOBJ;
+    openOBJ.open(filename);
+    
+    if (!openOBJ.good())
+    {
+        std::cout << "Can't open OBJ file" << std::endl;
+        exit(1);
+    }
+
+    // reading the file
+    while(!openOBJ.eof()) 
+    {
+
+        std::string line;
+        getline(openOBJ, line);
+        std::string objType = line.substr(0,2);
+    
+    // vertices
+    if (objType.compare("v ") == 0) 
+    {
+        // parse copied line
+        char* first = new char[line.size()+1];
+        memcpy(first, line.c_str(), line.size()+1);
+
+
+        // extract the tokens
+        Point3D vertex;
+        strtok(first, " "); // skips past the v, vt, vn or f part
+
+        // converting the contents of a string into a float value
+        vertex.x = std::stof(std::strtok(NULL, " "));
+        vertex.y = std::stof(std::strtok(NULL, " "));
+        vertex.z = std::stof(std::strtok(NULL, " "));
+
+        tempVertices.push_back(vertex);
+
+        delete[] first;
+        pos++;
+    } else if (objType.compare("vn ") == 0)
+    {
+        char* first = new char[line.size()+1];
+        memcpy(first, line.c_str(), line.size()+1);
+
+        Vec3D normal;
+        std::strtok(first, " ");
+
+        normal.x = std::stof(std::strtok(NULL, " "));
+        normal.y = std::stof(std::strtok(NULL, " "));
+        normal.z = std::stof(std::strtok(NULL, " "));
+
+        tempNormals.push_back(normal);
+
+        delete[] first;
+        norm++;
+
+    // faces
+    } else if (objType.compare("f ")== 0) 
+    {
+        char* first = new char[line.size()+1];
+        memcpy(first, line.c_str(), line.size()+1);
+
+        std::strtok(first, " ");
+        for (int i = 0; i <= 2; i++) 
+        {
+            vtxIndices.push_back(std::stoi(std::strtok(NULL, " /"))); // vertices
+            texIndices.push_back(std::stoi(std::strtok(NULL, " /"))); // textures
+            nIndices.push_back(std::stoi(std::strtok(NULL, " /"))); // normals
+        }
+        delete[] first;
+        face++;
+    }
+    }
+    openOBJ.close();
+    return true;
+}
--- ExtremeBowling/src/characters/asset.h
+++ ExtremeBowling/src/characters/asset.h
@@ -1,5 +1,6 @@
 #include "../misc/physics_3D.h"
-#include "../misc/objLoader.h"
+#include "../misc/mathlib_3D.h"
+#include <vector>
 
 #ifndef ASSET_H
 #define ASSET_H
@@ -7,12 +8,19 @@
 class Asset {
     protected:
         PhysicsObject3D physics;
-        // Mesh and texture here
+
     public:
         Asset(float startX, float startY, float startZ);
         float getX();
         float getY();
         float getZ();
+
+        std::vector <unsigned int> vtxIndices, texIndices, nIndices;
+        std::vector <Point3D> tempVertices; //Mesh
+        //std::vector <Point3D> tempUV; // Textures 
+        std::vector <Vec3D> tempNormals; // Normals
+
+        bool loadObj(const char* filename);
 };
 
 #endif
