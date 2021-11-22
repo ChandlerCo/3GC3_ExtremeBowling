62f6c795cc35ead6f4148b01078b924ed20d52d2
azadis2 <azadis2@mcmaster.ca>
2021-11-21 20:00:43 -0500

    potential lighting and segfault fix

--- ExtremeBowling/src/main.cpp
+++ ExtremeBowling/src/main.cpp
@@ -243,15 +243,15 @@ void display(void)
         0,1,0
     );
 
-    // glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
-    // glLightfv(GL_LIGHT0, GL_AMBIENT, amb);
-    // glLightfv(GL_LIGHT0, GL_DIFFUSE, diff);
-    // glLightfv(GL_LIGHT0, GL_SPECULAR, spec);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_pos);
+    glLightfv(GL_LIGHT0, GL_AMBIENT, amb);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, diff);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, spec);
     
-    // glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambMat2);
-    // glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffMat2);
-    // glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specMat2);
-    // glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 27);
+     glMaterialfv(GL_FRONT_AND_BACK, GL_AMBIENT, ambMat2);
+     glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, diffMat2);
+     glMaterialfv(GL_FRONT_AND_BACK, GL_SPECULAR, specMat2);
+     glMaterialf(GL_FRONT_AND_BACK, GL_SHININESS, 27);
     /*
     glPushMatrix();
     glTranslatef(20,0,20);
@@ -296,8 +296,7 @@ void display(void)
 }
 
 void init(){
-    glEnable(GL_LIGHTING);
-    glEnable(GL_LIGHT0);
+
     windowX = 800;
     windowY = 800;
     refreshRate = 120;
@@ -322,6 +321,8 @@ void init(){
 void handleReshape(int w, int h) {
     windowX = w;
     windowY = h;
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
     glViewport(0, 0, w, h);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
